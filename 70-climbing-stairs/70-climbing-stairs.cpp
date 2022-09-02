fc4cb992a17cd63dd8b09550ea695ac6b5840288
07-Pratibha <95922152+07-Pratibha@users.noreply.github.com>
2022-09-02 18:36:09 +0530

    Time: 6 ms (5.97%), Space: 6 MB (57.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,33 +1,50 @@
-// memoisation and recursive solution give runtime error;
+// // memoisation and recursive solution give runtime error;
 
-int climbStairs_helper(int n, int *ans) {
+// int climbStairs_helper(int n, int *ans) {
             
-    //base case
-    if(n < 0)
-        return 0;
-    if(n == 0)
-        return 1;
+//     //base case
+//     if(n < 0)
+//         return 0;
+//     if(n == 0)
+//         return 1;
     
-    if(ans[n] != -1)
-        return ans[n];
+//     if(ans[n] != -1)
+//         return ans[n];
 
-    int a = climbStairs_helper(n-1, ans); //when you take only 1 step first
-    int b = climbStairs_helper(n-2, ans); //when you take only 2 steps first
+//     int a = climbStairs_helper(n-1, ans); //when you take only 1 step first
+//     int b = climbStairs_helper(n-2, ans); //when you take only 2 steps first
     
-    ans[n] = a+b;
-    return ans[n];
+//     ans[n] = a+b;
+//     return ans[n];
 
-}
+// }
 
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         int *ans = new int[n+1];
+//         for(int i=0; i<=n; i++) {
+//             ans[i] = -1;
+//         }
+//         int res = climbStairs_helper(n, ans);
+//         return res;
+//     }
+
+// };
+
+
+//dp solution 
 class Solution {
 public:
     int climbStairs(int n) {
-        int *ans = new int[n+1];
-        for(int i=0; i<=n; i++) {
-            ans[i] = -1;
-        }
-        int res = climbStairs_helper(n, ans);
-        return res;
+        int ans[60];
+        
+        ans[0] = 1;
+        ans[1] = 1;
+        ans[2] = 2;
+        for(int i=3; i<=n; i++) 
+            ans[i] = ans[i-1] + ans[i-2];
+        
+        return ans[n];
     }
-
 };
