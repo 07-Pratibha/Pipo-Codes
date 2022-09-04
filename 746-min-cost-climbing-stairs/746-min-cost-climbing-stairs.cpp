71d7db997ceca8b5524272fc2ea2385b97bb539f
07-Pratibha <95922152+07-Pratibha@users.noreply.github.com>
2022-09-04 13:31:06 +0530

    Time: 3 ms (97.53%), Space: 13.8 MB (48.91%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,13 +1,33 @@
+
+
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        int dp[n];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<n;i++)
-            dp[i]=min(dp[i-2],dp[i-1])+cost[i];
+    int minCostClimbingStairs_helper(vector<int>& cost, int i, int *ans) {
+    if(i >= cost.size())
+        return 0;
+        
+    if(ans[i] != -1)
+        return ans[i];
+ 
+    int m = minCostClimbingStairs_helper(cost, i+1, ans);
+    int n = minCostClimbingStairs_helper(cost, i+2, ans);
+        
+    ans[i] = min(m, n) + cost[i];
     
-        return min(dp[n-1],dp[n-2]);
+    return ans[i];
+}
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int i;
+        int *ans = new int[cost.size() + 1];
+        
+        for(int i=0; i<=cost.size(); i++) {
+            ans[i] = -1;
+        }
+        int a = minCostClimbingStairs_helper(cost, 0, ans);
+        int b = minCostClimbingStairs_helper(cost, 1, ans);
+        
+        return min(a,b);        
     }
 };
