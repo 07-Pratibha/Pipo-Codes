6394b8114b31655ac19e637cb1bcfa0e2557066b
07-Pratibha <95922152+07-Pratibha@users.noreply.github.com>
2022-09-05 10:48:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,21 @@
- class Solution {
-public:
+//  class Solution {
+// public:
     
-    int climbStairs(int n) {
-	    if(n<1) 
-            return 0;
+//     int climbStairs(int n) {
+// 	    if(n<1) 
+//             return 0;
         
-        int a[100];
+//         int a[100];
         
-        a[0]=1;
-        a[1]=2;
+//         a[0]=1;
+//         a[1]=2;
         
-        for(int i=2;i<n;i++)
-            a[i]=a[i-1]+a[i-2];
+//         for(int i=2;i<n;i++)
+//             a[i]=a[i-1]+a[i-2];
         
-        return a[n-1];
-    }
-};
+//         return a[n-1];
+//     }
+// };
 
 // class Solution {
 // public:
@@ -37,32 +37,33 @@ public:
 
 //memoisation and recursive solution give runtime error;
 
-// int climbStairs_helper(int n, int *ans) {
-            
-//     //base case
-//     if(n == 0)
-//         return 0;
+int climbStairs_helper(int n, int *ans) {
+    if(n < 0)
+        return 0;            
+    //base case
+    if(n == 0)
+        return 1;
     
-//     if(ans[n] != -1)
-//         return ans[n];
+    if(ans[n] != -1)
+        return ans[n];
 
-//     int a = climbStairs_helper(n-1, ans); //when you take only 1 step first
-//     int b = climbStairs_helper(n-2, ans); //when you take only 2 steps first
+    int a = climbStairs_helper(n-1, ans); //when you take only 1 step first
+    int b = climbStairs_helper(n-2, ans); //when you take only 2 steps first
     
-//     ans[n] = a+b;
-//     return a+b;
+    ans[n] = a+b;
+    return a+b;
 
-// }
+}
 
-// class Solution {
-// public:
-//     int climbStairs(int n) {
-//         int *ans = new int[n+1];
-//         for(int i=0; i<=n; i++) {
-//             ans[i] = -1;
-//         }
-//         int res = climbStairs_helper(n, ans);
-//         return res;
-//     }
+class Solution {
+public:
+    int climbStairs(int n) {
+        int *ans = new int[n+2];
+        for(int i=0; i<=n; i++) {
+            ans[i] = -1;
+        }
+        int res = climbStairs_helper(n, ans);
+        return res;
+    }
 
-// };
+};
